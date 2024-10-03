195cc7d5e1152a5be9f9f954e7693e602309968f
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-10-03 07:16:51 +0100

    cleanup remove comments

--- src/lib.rs
+++ src/lib.rs
@@ -13,13 +13,21 @@ pub struct TimerFuture {
 
 /// Shared state between the future and the waiting thread
 struct SharedState {
-    /// Whether or not the sleep time has elapsed
     completed: bool,
-
-    /// The waker for the task that `TimerFuture` is running on.
-    /// The thread can use this after setting `completed = true` to tell
-    /// `TimerFuture`'s task to wake up, see that `completed = true`, and
-    /// move forward.
     waker: Option<Waker>,
 }
 
+impl Future for TimerFuture {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut shared_state = self.shared_state.lock().unwrap();
+
+        if shared_state.completed {
+            Poll::Ready(())
+        } else {
+            shared_state.waker = Some(cx.waker().clone());
+            Poll::Pending
+        }
+    }
+}
