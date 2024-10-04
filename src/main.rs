94c2eabd47ea32bf03c6d01c74faffcfce2b3630
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-10-04 16:58:28 +0100

    main.rs
    executor and spawner

---
+++ src/main.rs
@@ -0,0 +1,96 @@
+use futures::{
+    future::{BoxFuture, FutureExt},
+    task::{waker_ref, ArcWake},
+};
+use std::{
+    future::Future,
+    sync::mpsc::{sync_channel, Receiver, SyncSender},
+    sync::{Arc, Mutex},
+    task::Context,
+    time::Duration,
+};
+// The timer we wrote in the previous section:
+use timer_future::TimerFuture;
+
+struct Executor {
+    ready_queue: Receiver<Arc<Task>>,
+}
+
+#[derive(Clone)]
+struct Spawner {
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+struct Task {
+    future: Mutex<Option<BoxFuture<'static, ()>>>,
+    task_sender: SyncSender<Arc<Task>>,
+}
+
+impl ArcWake for Task {
+    fn wake_by_ref(arc_self: &Arc<Self>) {
+        let cloned = arc_self.clone();
+        arc_self
+            .task_sender
+            .send(cloned)
+            .expect("to many tasks queued");
+    }
+}
+
+fn new_executor_and_spawner() -> (Executor, Spawner) {
+    const MAX_QUEUED_TASKS: usize = 10_000;
+    let (task_sender, ready_queue) = sync_channel(MAX_QUEUED_TASKS);
+
+    (Executor { ready_queue }, Spawner { task_sender })
+}
+
+impl Spawner {
+    fn spawn(&self, future: impl Future<Output = ()> + 'static + Send) {
+        let future = future.boxed();
+        let task = Arc::new(Task {
+            future: Mutex::new(Some(future)),
+            task_sender: self.task_sender.clone(),
+        });
+
+        self.task_sender.send(task).expect("to many tasks queued");
+    }
+}
+
+impl Executor {
+    fn run(&self) {
+        while let Ok(task) = self.ready_queue.recv() {
+            println!("task exist");
+            let mut future_slot = task.future.lock().unwrap();
+            if let Some(mut future) = future_slot.take() {
+                let waker = waker_ref(&task);
+
+                let context = &mut Context::from_waker(&waker);
+
+                if future.as_mut().poll(context).is_pending() {
+                    *future_slot = Some(future);
+                }
+            }
+        }
+    }
+}
+
+fn main() {
+    let (executor, spawner) = new_executor_and_spawner();
+
+    spawner.spawn(async {
+        println!("howdy!");
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("done");
+    });
+
+    spawner.spawn(async {
+        println!("howdy-0!");
+        TimerFuture::new(Duration::new(4, 0)).await;
+        println!("done-0");
+    });
+
+    // Drop the spawner so that our executor knows it is finished and won't
+    // receive more incoming tasks to run.
+    drop(spawner);
+
+    executor.run();
+}
